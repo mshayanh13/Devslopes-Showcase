0d17cec34c1da6e7b87d781d5443633a91d69bf4
mshayanh13 <mohammadhemani@gmail.com>
2017-03-15 18:40:15 -0500

    Login User Inteface

---
+++ devslopes-showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  devslopes-showcase
+//
+//  Created by Mohammad Hemani on 3/10/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
